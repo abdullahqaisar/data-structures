ec9af383ae99d094e999bd2361d4f7486c64a30d
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-14 22:33:42 +0500

    Prime Number

--- Udacity - Data Structures & Algorithms/fibonacci.py
+++ Udacity - Data Structures & Algorithms/fibonacci.py
@@ -5,7 +5,7 @@ iterative code in the instructions."""
 
 def get_fib(position):
 
-    if position <= 1:
+    if position = 1:
         return position
     else:
         return (get_fib(position - 1) + get_fib(position - 2))
